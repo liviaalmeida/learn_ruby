5678812c9d2c6ab77298464518bca42a23796084
livia <leave.ah@gmail.com>
2017-02-09 15:44:26 -0200

    Done number 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title=(title)
+		nope = ["and","over","the","a","in","of","an"]
+		arr = title.split(" ")
+		if arr.length==1
+			@title = arr[0].capitalize
+		else
+			@title = arr[0].capitalize+" "+(arr[1..arr.length].map { |a| (nope.grep(a)==[]) ? a.capitalize : a }).join(" ")
+		end
+	end
+
 end
