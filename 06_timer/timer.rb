8bbda677def249251e60b9fcd2ca4a223cb8a9fa
liviaalmeida <leave.ah@gmail.com>
2017-02-09 16:05:54 -0200

    Finished

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
-  #write your code here
+#write your code here
+	attr_accessor :seconds
+
+	def initialize()
+		@seconds = 0
+	end
+
+
+
+	def time_string
+		hr = @seconds/3600
+		mn = (@seconds%3600) / 60
+		sc = (@seconds%3600)%60
+		str = lambda { |num| return (num<10 ? "0"+String(num) : String(num))  }
+		return [str[hr],str[mn],str[sc]].join(":")
+	end
 end
